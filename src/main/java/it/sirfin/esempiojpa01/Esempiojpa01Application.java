2a85b528a2199bc6cc4e2eebd766be7544c422c9
luca-lezzerini <47611629+luca-lezzerini@users.noreply.github.com>
2021-02-11 09:47:23 +0100

    Primo commit

---
+++ src/main/java/it/sirfin/esempiojpa01/Esempiojpa01Application.java
@@ -0,0 +1,13 @@
+package it.sirfin.esempiojpa01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esempiojpa01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esempiojpa01Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/sirfin/esempiojpa01/Esempiojpa01ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.esempiojpa01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esempiojpa01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
